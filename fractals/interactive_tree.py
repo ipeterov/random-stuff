c530e410f6998eb8a01343d3fc879b54de18eb3b
Ilya Peterov <ipeterov1@gmail.com>
2019-09-24 18:12:04 +0300

    Fractal interactive tree - more reasonable defaults

--- fractals/interactive_tree.py
+++ fractals/interactive_tree.py
@@ -1,16 +1,28 @@
 import pygame
 
-from tree import TreeDrawer, DEFAULT_PARAMS
+from tree import TreeDrawer
 
-SIZE = WIDTH, HEIGHT = 1700, 1400
-BLACK = 0, 0, 0
+
+SIZE = WIDTH, HEIGHT = 500, 500
+BACKGROUND_COLOR = 0, 0, 0
+DEFAULT_PARAMS = {
+    'start': (WIDTH / 2, HEIGHT),
+    'starting_length': 100,
+    'starting_thickness': 10,
+    'thickness_multiplier': 0.7,
+    'length_multiplier': 0.7,
+    'angle_1': -60,
+    'angle_2': 40,
+    'max_depth': 7,
+}
+
 
 
 def edit_param(params, param_name, key_increase, key_decrease):
     if pygame.key.get_pressed()[key_increase]:
-        params[param_name] *= 1.05
+        params[param_name] *= 1.001
     elif pygame.key.get_pressed()[key_decrease]:
-        params[param_name] /= 1.05
+        params[param_name] /= 1.001
 
 
 if __name__ == '__main__':
@@ -38,7 +50,7 @@ if __name__ == '__main__':
         if pygame.key.get_pressed()[pygame.K_SPACE]:
             params = DEFAULT_PARAMS.copy()
 
-        screen.fill(BLACK)
+        screen.fill(BACKGROUND_COLOR)
         tree_drawer.set_params(params)
         tree_drawer.draw_tree(screen)
 
